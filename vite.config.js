438139d5adbc4ba727a3c1a2866a75809da187ff
FabsHazard <fcriveros@uc.cl>
2023-12-08 15:34:47 -0300

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-export default ({ mode }) => {
+export default defineConfig((mode) => {
   const env = loadEnv(mode, process.cwd());
 
-  return defineConfig({
-    base: "/LoteriaDApp",
+  return {
+    base: "/LoteriaDApp/",
     plugins: [react()],
     server: {
-      port: parseInt(env.VITE_APP_PORT),
+      port: env.VITE_APP_PORT,
     },
     resolve: {
       alias: {
@@ -21,5 +20,5 @@ export default ({ mode }) => {
         '@utils': fileURLToPath(new URL('./src/utils', import.meta.url))
       }
     }
-  });
-};
+  }
+});
